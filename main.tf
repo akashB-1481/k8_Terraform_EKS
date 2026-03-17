ce13bf0941a34c26290cf7bbb6d627f80f81bf0d
Ab1las7 <abilashbm7@gmail.com>
2026-03-17 14:01:03 +0530

    resource subnet created

--- main.tf
+++ main.tf
@@ -7,16 +7,29 @@ terraform {
   }
 }
 
+#adding profile to mentione the iam i want to use
 provider "aws" {
   region = var.region
   profile = "devops"
 }
 
+#create a VPC 
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = var.var_terra.vpc_cidr
+  cidr_block = var.var_terra.vpc_cidr #mention the CIDR block 
   tags = {
   Name = "devops-vpc"
 }
 enable_dns_support   = true
 enable_dns_hostnames = true
+}
+
+#create subnet for the above VPC 
+resource "aws_subnet" "dev_net" {
+    vpc_id = aws_vpc.dev_vpc.id #mentino the VPC ID
+    cidr_block = var.var_terra.public_subnet_cidr # creat the cidr block
+    availability_zone = var.var_terra.az #adding the availablity zone
+    tags =  {
+        Name = "devops-subnet-block"
+    }
+  
 }
