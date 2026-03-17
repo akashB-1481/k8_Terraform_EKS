b389f12d454b20e0a20b8c3cd864aa3e85d555c9
Ab1las7 <abilashbm7@gmail.com>
2026-03-17 16:09:54 +0530

    change in eks file

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "dev_eks" {
     version = "1.35"
     role_arn = aws_iam_role.eks_cluster_role.arn 
     vpc_config {
-      subnet_ids = [aws_subnet.dev_net_1.id ,aws_subnet.dev_net_2 ] 
+      subnet_ids = [aws_subnet.dev_net_1.id ,aws_subnet.dev_net_2.id ] 
     }
   tags = {
     name = "devops-eks"
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "dev_eks_node" {
       cluster_name    = aws_eks_cluster.dev_eks.name
       node_group_name = "devops-eks-node"
       node_role_arn   = aws_iam_role.eks_node_role.arn
-      subnet_ids      = [aws_subnet.dev_net_1.id , aws_subnet.dev_net_2]
+      subnet_ids      = [aws_subnet.dev_net_1.id , aws_subnet.dev_net_2.id]
 
       scaling_config {
         min_size = 1
