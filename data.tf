faf673cff29476d29c4cffb9b2aa7fdf107316da
Ab1las7 <abilashbm7@gmail.com>
2026-03-23 22:01:46 +0530

    data file correction

--- data.tf
+++ data.tf
@@ -1,1 +1,17 @@
 # removing data for the time being 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
